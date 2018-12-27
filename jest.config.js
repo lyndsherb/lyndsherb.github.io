f8544d2c802aa64f95ce409d3528498bc3fc5515
Lyndsey Herbert <lyndsherb@gmail.com>
2018-12-27 00:51:03 +0000

    UPDATED jest config location

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+    "clearMocks": true,
     "transform": {
-      "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
+      "^.+\\.jsx?$": "<rootDir>/tests/config/jest-preprocess.js"
     },
     "moduleNameMapper": {
       ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-      ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+      ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/__mocks__/fileMock.js"
     },
     "testPathIgnorePatterns": ["node_modules", ".cache"],
     "transformIgnorePatterns": ["node_modules/(?!(gatsby)/)"],
@@ -12,5 +13,5 @@ module.exports = {
       "__PATH_PREFIX__": ""
     },
     "testURL": "http://localhost",
-    "setupFiles": ["<rootDir>/loadershim.js"]
+    "setupFiles": ["<rootDir>/tests/config/loadershim.js"]
   }
