201920aab2ba76d46f10646f8f4dba048a4315ee
Lyndsey Herbert <lyndsherb@gmail.com>
2018-12-29 12:33:26 +0000

    ADDED new components, ADDED styled-components

---
+++ src/components/styled/body.js
@@ -0,0 +1,12 @@
+/**
+ * Styled components - Body
+ */
+
+import styled from 'styled-components';
+
+const Body = styled.body`
+    font-family: 'sans-serif';
+    font-size: 16px;
+`;
+
+export default Body;
---
+++ src/components/styled/container.js
@@ -0,0 +1,13 @@
+/**
+ * Styled components - container
+ */
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 0 1.0875rem 1.45rem;
+`;
+
+export default Container;
