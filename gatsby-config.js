96c2d945e91b36c82285177eb274e7bf5807219c
Lyndsey Herbert <lyndsherb@gmail.com>
2018-12-31 19:39:19 +0000

    UPDATED config, ADDED basic stuff for styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shadow and Fox`,
+    description: `The blog and portfolio site of Lyndsey Herbert, a Senior Front-end Developer currently working at Ampersand Commerce.`,
+    author: `@lyndsherb`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3CAEB4`,
+        theme_color: `#3CAEB4`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
